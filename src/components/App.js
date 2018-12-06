483b72e905f7b6faa6038a20983c6f7cf1c5bf4c
TTmM <tm@iMac-TM.local>
2018-12-06 11:08:07 +0100

    header section added

--- src/components/App.js
+++ src/components/App.js
@@ -16,34 +16,14 @@ import Footer from './Footer';
 class App extends Component {
     render() {
         return (
-            <HashRouter>
-                <div>
-                    <nav>
-                        <ul className='nav'>
-                            // sprubowac ostylowac NAvLink nie po klasie a sam tag a ???
-                            <li className='nav-link'><NavLink className='nav-link-a' activeClassName='activeNavLink' exact
-                                                              to='/'>Start </NavLink></li>
-                            <li className='nav-link'><NavLink className='nav-link-a'
-                                                                            activeClassName='activeNavLink' exact
-                                                                            to='/ulubione'>Ulubione</NavLink></li>
-                        </ul>
-                    </nav>
-                    <Info/>
-                    <FourEasySteps/>
-                    <About/>
-                    <FundationsList/>
-                    <Footer/>
-                    <Switch>
-                        <Route exact path='/ulubione'
-                               render={(props) => <Header {...props}/>}
-                        />
-                        {/*<Route exact path='/' component={Home}/>*/}
-
-
-                    </Switch>
-
-                </div>
-            </HashRouter>
+            <div>
+                <Header/>
+                <Info/>
+                <FourEasySteps/>
+                <About/>
+                <FundationsList/>
+                <Footer/>
+            </div>
         );
     }
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,95 @@
 import React from 'react';
 
-const Header = () => {
-    return <div>
-        testcik headera komponentu
-    </div>
+class Header extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            isMobileView: '',
+            isMenuVisible: ''
+
+        }
+    }
+
+    componentDidMount(){
+      console.log('komponenet');
+        const mobile = window.matchMedia("screen and (max-width: 400px)");
+        if (mobile.matches) {
+            this.setState({
+                isMobileView: true,
+                isMenuVisible: false
+            })
+        } else  {
+            this.setState({
+                isMobileView: false,
+                isMenuVisible: true
+
+            })
+        }
+        mobile.addListener( (mobile) => {
+            if (mobile.matches) {
+console.log('mobile');
+                this.setState({
+                    isMobileView: true,
+                    isMenuVisible: false
+                })
+            } else  {
+                this.setState({
+                    isMobileView: false,
+                    isMenuVisible: true
+                })
+            }
+        });
+    };
+
+    handleHamburgerClick = () => {
+      this.setState({
+          isMenuVisible: !this.state.isMenuVisible
+      })
+    };
+
+    render() {
+        console.log(this.state.isMobileView);
+
+        const hamburgerIcon = this.state.isMenuVisible ? <i onClick={this.handleHamburgerClick} className="fas fa-times"/> :  <i onClick={this.handleHamburgerClick} className="fas fa-bars"/>;
+        return (
+            <section id='header-section'>
+                <div style={{display: this.state.isMobileView ? 'none': 'block'}}>TEST</div>
+                <header>
+                    <div className='login-section'>
+                        <span>Zaloguj</span>
+                        <span>Załóż konto</span>
+                    </div>
+                    <nav>
+                        <div style={{display: this.state.isMobileView ? 'flex' : 'none'}} className='mobileMenu'><i className="fas fa-tshirt"/> {hamburgerIcon}
+                        </div>
+                        <ul style={{display: this.state.isMenuVisible ? 'block' : 'none'}} className={this.state.isMenuVisible ? 'openMenu':'closeMenu'}>
+                            <li><a href='#header-section'>Start</a></li>
+                            <li><a href='#fourEasySteps-section'>O co chodzi?</a></li>
+                            <li><a href='#about-section'>O nas</a></li>
+                            <li><a href='#fundationsList-section'>Fundacje i organizacje</a></li>
+                            <li><a href='#footer-section'>Kontakt</a></li>
+                        </ul>
+
+                    </nav>
+                </header>
+                <div className='mainIdea'>
+                    <h1>
+                        <span>Zacznij pomagać!</span>
+                        <span>Oddaj niechciane rzeczy w zaufane ręce</span>
+                    </h1>
+                    <div className='header-decoration'/>
+                    <div className='mainButtons'>
+                        <button><span>Oddaj</span> <span>rzeczy</span></button>
+                        <button><span>Zorganizuj</span> <span>zbiórkę</span>
+                        </button>
+                    </div>
+                </div>
+
+
+
+            </section>);
+    }
 };
 
 export default Header;
